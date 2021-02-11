3e0da733863aa1a8981dbdce4aa5efee4cec2916
EmmyM_ighty <remi.emma04@gmail.com>
2021-02-11 01:34:05 +0100

    Fixed the required issue, as well as enabled automatic reload after close of modal

--- public/assets/app.js
+++ public/assets/app.js
@@ -6,52 +6,62 @@ $('#searchForm').on('submit', async function (e) {
     const $bedrooms = $('#InputBedrooms').val();
     const $toilets = $('#InputToilets').val();
 
-    const predictor = {
-        $location,
-        $apartment,
-        $bedrooms,
-        $toilets
-    }
+    let predictor = {}
 
-    $('#InputLocation').val('');
-    $('#InputApartment').val('Type of apartment');
-    $('#InputBedrooms').val('No of bedrooms');
-    $('#InputToilets').val('No of toilets');
-
-    // const feedback = await fetch('http://localhost:3000/', {
-    const feedback = await fetch('https://rentright.herokuapp.com/', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(predictor)
-    }).then(res => res.json())
-        .then(data => {
-            return data;
-        });
-
-
-    if (feedback) {
-        let myPrice;
-        for (let key in feedback) {
-            myPrice = feedback[key];
+    if ($location !== 'Location' && $apartment !== 'Type of apartment' && $bedrooms !== 'No of bedrooms' && $toilets !== 'No of bathrooms / toilets') {
+        predictor = {
+            $location,
+            $apartment,
+            $bedrooms,
+            $toilets
         }
 
-        let location = "Location: " + $location;
-        let type = "Apartment Type: " + $apartment;
-        let bedrooms = "Number of Bedrooms: " + $bedrooms;
-        let toilets = "Number of Bathrooms / Toilets: " + $toilets;
+        $('#InputLocation').val('Location');
+        $('#InputApartment').val('Type of apartment');
+        $('#InputBedrooms').val('No of bedrooms');
+        $('#InputToilets').val('No of bathrooms / toilets');
 
-        let lowerPrice = (myPrice - 10000).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
-        let upperPrice = (myPrice + 10000).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+        const feedback = await fetch('http://localhost:3000/', {
+        // const feedback = await fetch('https://rentright.herokuapp.com/', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(predictor)
+        }).then(res => res.json())
+            .then(data => {
+                return data;
+            });
 
-        $('.apartLoc').text(location)
-        $('.apartType').text(type)
-        $('.bedrooms').text(bedrooms)
-        $('.toilets').text(toilets)
 
-        $('.lower').text(lowerPrice);
-        $('.upper').text(upperPrice);
-        $('#exampleModal').modal({ show: true });
+        if (feedback) {
+            let myPrice;
+            for (let key in feedback) {
+                myPrice = feedback[key];
+            }
+
+            let location = "Location: " + $location;
+            let type = "Apartment Type: " + $apartment;
+            let bedrooms = "Number of Bedrooms: " + $bedrooms;
+            let toilets = "Number of Bathrooms / Toilets: " + $toilets;
+
+            let lowerPrice = (myPrice - 10000).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+            let upperPrice = (myPrice + 10000).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+
+            $('.apartLoc').text(location)
+            $('.apartType').text(type)
+            $('.bedrooms').text(bedrooms)
+            $('.toilets').text(toilets)
+
+            $('.lower').text(lowerPrice);
+            $('.upper').text(upperPrice);
+            $('#exampleModal').modal({ show: true });
+        }
+    } else {
+        $('.modal-body').text('Please Fill The Form...');
     }
 });
+
+$('#closeBtn').on('click', function() {
+    location.reload()
+})
